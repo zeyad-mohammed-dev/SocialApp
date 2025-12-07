fc5d249d8c12f4b0a305732611260254acc26e88
Zeyad Mohammed <zeyadmohammed200493@gmail.com>
2025-12-07 14:34:47 +0200

    Remove dist folder from tracking

--- dist/modules/user/user.authorization.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endpoint = void 0;
-const User_model_1 = require("../../DB/models/User.model");
-exports.endpoint = {
-    profile: [User_model_1.RoleEnum.user],
-    restoreAccount: [User_model_1.RoleEnum.admin],
-    hardDeleteAccount: [User_model_1.RoleEnum.admin],
-};
--- dist/utils/response/success.response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.successResponse = void 0;
-const successResponse = ({ res, message = 'Done', statusCode = 200, data, }) => {
-    return res.status(statusCode).json({ message, statusCode, data });
-};
-exports.successResponse = successResponse;
